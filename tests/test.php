35ee7d648d845573022740613ab3d2fb238591b8
Francisco Catarrinho <francisco.catarrinho1@gmail.com>
2017-10-28 16:17:12 +0100

    Fixed command line arguments

--- tests/test.php
+++ tests/test.php
@@ -5,11 +5,11 @@ require_once('../src/Client.php');
 
 use Mailman2Wrapper\Client;
 
-if(!isset($argv[1] || !isset($argv[2]) || !isset($argv[3]))){
+if(!isset($argv[1]) || !isset($argv[2]) || !isset($argv[3])){
     die("Usage: test.php host group password");
 }
 $HOST = $argv[1];
 $GROUP = $argv[2];
-$PASSWORD = $argv[2];
+$PASSWORD = $argv[3];
 
 $client = new Client($HOST, $GROUP, $PASSWORD);
