58bd32dd008e1bd636addb45a4665bb9a93df648
Luis Ávila <filipe@avilaesousa.com>
2017-10-28 01:30:30 +0100

    Added subscribe functionality and getCSRF for operations that need one.

--- src/Client.php
+++ src/Client.php
@@ -29,5 +29,43 @@ class Client {
         $this->curl->setCookie('groups.neiist.rp+admin', $this->curl->getCookie('groups.neiist.rp+admin'));
         return true;
     }
+
+    public function subscribe($who, $welcome = true, $notify = false){
+        if(!is_array($who)) $who = array($who);
+        // We need a dummy file to send a multipart/form-data
+        touch('dummy');
+        $data = array(
+            'csrf_token' => $this->getCSRF($this->host . '/admin/' . $this->group . '/members/add'),
+            'subscribees' => join('\n', $who),
+            'subscribees_upload' => new \CURLFile('dummy'),
+            'subscribe_or_invite' => 0,
+            'send_welcome_msg_to_this_batch' => $welcome,
+            'send_notifications_to_list_owner' => $notify,
+            'setmemberopts_btn' => 'Submit your changes'
+        );
+        $this->curl->setHeader('Content-Type', 'multipart/form-data');
+        $this->curl->post($this->host . '/admin/' . $this->group . '/members/add', $data);
+        // Let's restore things to how they were
+        unlink('dummy');
+        $this->curl->setHeader('Content-Type', 'application/x-www-form-urlencoded');
+        // Handle 401, 403, 500
+        if($this->curl->error){
+            throw new Exception($this->curl->httpErrorMessage, $this->curl->httpError);
+        }
+        return true;
+    }
+
+    private function getCSRF($where){
+        $this->curl->get($where);
+        if($this->curl->error){
+            throw new Exception($this->curl->errorMessage, $this->curl->errorCode);
+            throw new Exception($this->curl->httpErrorMessage, $this->curl->httpError);
+        }
+        if(preg_match('/<input.*?name=(\'|")csrf_token("|\'").*?>/', $this->curl->response, $match)){
+            if(preg_match('/value=(?:\'|")(.*?)(?:\'|")/', $match[0], $match)){
+                return $match[1];
+            }
+        }
+        return false;
     }
 }
